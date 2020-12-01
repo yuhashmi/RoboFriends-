298f2d52ec81484727cf6749a7fba39b8d9cdc6c
yuhashmi <giogiosef97@gmail.com>
2020-12-01 19:34:20 +0000

    removed <div></div> wrapping to avoid extra nesting, instead using React Fragment method

--- src/Card.js
+++ src/Card.js
@@ -1,17 +1,17 @@
 import React from 'react';
 
-const Card = (props) => {
+const Card = ({ name, email, id }) => {
     return (
         // className for styling the cards using tachyons
-        <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+        <dl className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
         {/* img is sourcing robots photo from robohash */}
-            <img alt='robots' src={`https://robohash.org/${props.id}?200x200`} />
-            <div>
+            <img alt='robots' src={`https://robohash.org/${id}?200x200`} />
+            <dl>
             {/* name and email referenced from index.js as properties will be displayed on cards */}
-                <h2>{props.name}</h2>
-                <p>{props.email}</p>
-            </div>
-        </div>
+                <h2>{name}</h2>
+                <p>{email}</p>
+            </dl>
+        </dl>
     );
 }
 
--- src/index.js
+++ src/index.js
@@ -8,11 +8,11 @@ import 'tachyons';
 
 ReactDOM.render(
   <React.StrictMode>
-    <div>
+    <dl>
       <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
       <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
       <Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
-    </div>
+    </dl>
   </React.StrictMode>,
   document.getElementById('root')
 );

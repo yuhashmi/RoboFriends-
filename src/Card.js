8516afc1f73d90348f5758a3b7fe63269db2e5a1
yuhashmi <giogiosef97@gmail.com>
2020-12-01 19:20:13 +0000

    different robots now for each ID

--- src/Card.js
+++ src/Card.js
@@ -2,9 +2,12 @@ import React from 'react';
 
 const Card = (props) => {
     return (
-        <div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-            <img alt='robots' src='https://robohash.org/test?200x200' />
+        // className for styling the cards using tachyons
+        <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+        {/* img is sourcing robots photo from robohash */}
+            <img alt='robots' src={`https://robohash.org/${props.id}?200x200`} />
             <div>
+            {/* name and email referenced from index.js as properties will be displayed on cards */}
                 <h2>{props.name}</h2>
                 <p>{props.email}</p>
             </div>

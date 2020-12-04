1cc5644fc6cce3fe843abbe0411126f8ae9cf306
yuhashmi <giogiosef97@gmail.com>
2020-12-04 17:05:49 +0000

    added cardlist to work with the list in seperate file

--- src/index.js
+++ src/index.js
@@ -1,18 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Card from './Card';
 import reportWebVitals from './reportWebVitals';
 import { robots } from './robots'
 import 'tachyons';
+import CardList from './CardList';
 
 ReactDOM.render(
   <React.StrictMode>
-    <dl>
-      <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
-      <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
-      <Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
-    </dl>
+    <CardList robots={robots}/>
   </React.StrictMode>,
   document.getElementById('root')
 );

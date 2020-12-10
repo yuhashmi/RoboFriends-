82ee0d58d6197e299d3ab2d180a6ceb31f075e52
yuhashmi <giogiosef97@gmail.com>
2020-12-10 09:12:38 +0000

    scroll component added so the only profile part of website is scrolled

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
+import Scroll from './Scroll';
 import './App.css';
 
 class App extends Component {
@@ -33,7 +34,9 @@ class App extends Component {
                 <dl className='tc'>
                     <h1 className='f1'>RoboFriends</h1>
                     <SearchBox searchChange={this.onSearchChange}/>
+                    <Scroll>
                     <CardList robots={filteredRobots}/>
+                    </Scroll>
                 </dl>
             );
         }  
